536a00a0836758b6adb594991680b1ed5136d6b8
Nguyen Huu Tri <nguyenhuutri0905@gmail.com>
2023-02-19 15:13:13 +0700

    init project

---
+++ frontend/src/components/Dogs/DogCard.jsx
@@ -0,0 +1,45 @@
+import { useContext, useState } from "react";
+import { CartContext } from "../../Context/CartContext";
+import "./dogs.css";
+
+const DogCard = (props) => {
+  const { id, name, breed, description, price, imageUrl } = props;
+  const { addToCart, setTotal } = useContext(CartContext);
+  const [isAdded, setAdded] = useState(false);
+  const handleclick = () => {
+    setAdded(true);
+    const newItems = {
+      name: name,
+      price: price,
+      imageUrl: imageUrl,
+    };
+    addToCart((item) => [...item, newItems]);
+    setTotal((total) => (total += Number(price)));
+  };
+  return (
+    <>
+      <section className="dogs">
+        <div className="dogs-info">
+          <p>{name}</p>
+          <p>{breed}</p>
+        </div>
+        <div className="dogs-img-container">
+          <img className="dog-img" src={imageUrl} alt={`picture of: ${name}`} />
+        </div>
+        <div className="dogs-desc">{description}</div>
+        <div className="dogs-price">{price}$</div>
+        {isAdded ? (
+          <button disabled className="dogs-btn-disabled">
+            ADDED
+          </button>
+        ) : (
+          <button className="dogs-btn" onClick={handleclick}>
+            ADD TO CART
+          </button>
+        )}
+      </section>
+    </>
+  );
+};
+
+export default DogCard;
